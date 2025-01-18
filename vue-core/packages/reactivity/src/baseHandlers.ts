d0a04fb048830c4682aabf13bbfaadafbafef1bb
majuntao <1547996098@qq.com>
2025-01-18 12:50:42 +0800

    get set

--- vue-core/packages/reactivity/src/baseHandlers.ts
+++ vue-core/packages/reactivity/src/baseHandlers.ts
@@ -1,2 +1,31 @@
 // ProxyHandler ts中的类型
-export const mutableHandlers: ProxyHandler<object> = {}
+
+import { track, trigger } from './effect'
+
+function createGetter() {
+  return function get(target: object, key: string | symbol, reactiver: object) {
+    const res = Reflect.get(target, key, reactiver)
+    track(target, key)
+    return res
+  }
+}
+
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    reactiver: object
+  ) {
+    const result = Reflect.set(target, key, value, reactiver)
+    trigger(target, key, value)
+    return result
+  }
+}
+const set = createSetter()
+const get = createGetter()
+
+export const mutableHandlers: ProxyHandler<object> = {
+  set,
+  get
+}
---
+++ vue-core/packages/reactivity/src/effect.ts
@@ -0,0 +1,13 @@
+/**
+ * 触发依赖
+ * @param target
+ * @param key
+ */
+export function trigger(target: object, key: unknown, value: unknown) {}
+
+/**
+ * 收集依赖
+ * @param target
+ * @param key
+ */
+export function track(target: object, key: unknown) {}
